3702154c726ebfb84bd22bfdd91baa3b79a83fc3
skchel174 <skchel174@gmail.com>
2020-09-15 20:04:35 +0500

    class Word

---
+++ Word.php
@@ -0,0 +1,15 @@
+<?php
+
+class Word {
+    /** string $name **/
+    public $name;
+    /** string $translation **/
+    public $translation;
+    /** string $transcription **/
+    public $transcription;
+    /** string $description **/
+    public $description;
+    /** string $examples **/
+    public $examples;
+}
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once './Word.php';
+
+$house = new Word();
+
+$house->name = 'house';
+$house->translation = 'дом';
+$house->transcription = 'haʊs';
+$house->description = 'A house is a single-unit residential building, which may range in complexity from a rudimentary hut to a complex, structure of wood, masonry, concrete or other material, outfitted with plumbing, electrical, and heating, ventilation, and air conditioning systems.';
+$house->examples = '1) Montag had rarely seen that many house lights. 2) The house was so small, the furnishings so very simple.';
+
+echo $house->name . PHP_EOL;
+echo $house->transcription . PHP_EOL;
+echo $house->translation . PHP_EOL;
+echo $house->description . PHP_EOL;
+echo $house->examples . PHP_EOL;
