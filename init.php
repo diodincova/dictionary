b1f7786be0cddbb363d75687a3d6ae2660e0906b
skchel174 <skchel174@gmail.com>
2020-09-28 13:40:59 +0500

    issue3

--- init.php
+++ init.php
@@ -1,31 +1,36 @@
 <?php
-require_once './Word.php';
-require_once './load.php';
 
-$house = new Word();
+require_once './class/Word.php';
+require_once './class/Load.php';
 
-$house->name = 'house';
-$house->translation = 'дом';
-$house->transcription = 'haʊs';
-$house->description = 'A house is a single-unit residential building, which may range in complexity from a rudimentary hut to a complex, structure of wood, masonry, concrete or other material, outfitted with plumbing, electrical, and heating, ventilation, and air conditioning systems.';
-$house->examples = [
-    '1) Montag had rarely seen that many house lights.', 
-    '2) The house was so small, the furnishings so very simple.',
-];
+$house = new Word(
+    'house',
+    'дом',
+    '[haʊs]',
+    'A house is a single-unit residential building, which may range in complexity from a rudimentary hut to a complex, structure of wood, masonry, concrete or other material, outfitted with plumbing, electrical, and heating, ventilation, and air conditioning systems.',
+    [
+        '1) Montag had rarely seen that many house lights.', '2) The house was so small, the furnishings so very simple.',
+    ]
+);
 
-$tree = new Word();
-
-$tree->name = 'tree';
-$tree->translation = 'дерево';
-$tree->transcription = 'tri';
-$tree->description = 'In botany, a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species. In some usages, the definition of a tree may be narrower, including only woody plants with secondary growth, plants that are usable as lumber or plants above a specified height. In wider definitions, the taller palms, tree ferns, bananas, and bamboos are also trees.';
-$tree->examples = [
-    '1) Fruit grows on trees.', 
-    '2) An apple fell from the tree.', 
-    '3) The cherry tree bloomed.',
-];   
+$tree = new Word(
+    'tree',
+    'дерево',
+    '[tri]',
+    'In botany, a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species. In some usages, the definition of a tree may be narrower, including only woody plants with secondary growth, plants that are usable as lumber or plants above a specified height. In wider definitions, the taller palms, tree ferns, bananas, and bamboos are also trees.',
+    [
+        '1) Fruit grows on trees.', 
+        '2) An apple fell from the tree.', 
+        '3) The cherry tree bloomed.',
+    ]
+);
 
 $house->save();
 $tree->save();
 
-loadDictionaryWords();
+$load = new Load();
+
+$wordObj = $load->getWord('tree');
+$wordObj->print();
+
+
--- load.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function loadDictionaryWords() {
-    $words = json_decode(file_get_contents('words.json'));
-    foreach ($words as $word) {
-        $word = unserialize($word);
-        $word->print();
-    }
-}
