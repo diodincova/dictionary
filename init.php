493048a1cc5903de668968e23dbfe2f156278014
Diana <di_07@inbox.ru>
2020-10-04 17:55:56 +0200

    небольшие изменения данных

--- init.php
+++ init.php
@@ -6,22 +6,23 @@ require_once './class/Load.php';
 $house = new Word(
     'house',
     'дом',
-    '[haʊs]',
+    'haʊs',
     'A house is a single-unit residential building, which may range in complexity from a rudimentary hut to a complex, structure of wood, masonry, concrete or other material, outfitted with plumbing, electrical, and heating, ventilation, and air conditioning systems.',
     [
-        '1) Montag had rarely seen that many house lights.', '2) The house was so small, the furnishings so very simple.',
+        'Montag had rarely seen that many house lights.',
+        'The house was so small, the furnishings so very simple.',
     ]
 );
 
 $tree = new Word(
     'tree',
     'дерево',
-    '[tri]',
+    'tri',
     'In botany, a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species. In some usages, the definition of a tree may be narrower, including only woody plants with secondary growth, plants that are usable as lumber or plants above a specified height. In wider definitions, the taller palms, tree ferns, bananas, and bamboos are also trees.',
     [
-        '1) Fruit grows on trees.', 
-        '2) An apple fell from the tree.', 
-        '3) The cherry tree bloomed.',
+        'Fruit grows on trees.', 
+        'An apple fell from the tree.', 
+        'The cherry tree bloomed.',
     ]
 );
 
@@ -32,5 +33,4 @@ $load = new Load();
 
 $wordObj = $load->getWord('tree');
 $wordObj->print();
-
 
